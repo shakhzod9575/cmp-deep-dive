a42349451c5f8c94ce4d35ff378b1d05a357007f
shakhzod9575 <shahzodelturayevepam@gmail.com>
2025-12-22 21:56:15 +0500

    Learn host element

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, ViewEncapsulation, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None // Makes control css like global because it use projection so it only effect to current
 })
 export class ControlComponent {
   label = input.required<string>();
