a16c7fdd76196d1093c92e70e3277831a4f40ba9
Shakhzod Elturaev <shahzodelturayevepam@gmail.com>
2025-12-24 11:10:53 +0500

    Added interval on ngOnInit

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, ViewEncapsulation, input } from '@angular/core';
+import { Component, ElementRef, ViewEncapsulation, inject, input } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,20 @@ import { Component, HostBinding, ViewEncapsulation, input } from '@angular/core'
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None, // Makes control css like global because it use projection so it only effect to current
   host: {
-    class: 'control'
-  }
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
 export class ControlComponent {
   // @HostBinding('class') className = 'control';
+  // @HostListener('click') onClick() {
+  //   console.log('Clicked!');
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+  onClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+  }
 }
